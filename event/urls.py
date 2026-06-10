7f81bc350664f10d80c14808bbc0f2af295773c1
Chetan Spectacom <chetan.chauhan@spectacom.in>
2026-06-10 14:19:44 +0530

    health check added

--- event/urls.py
+++ event/urls.py
@@ -5,6 +5,7 @@ from .api_views import SearchByFaceAPIView, UploadPhotoAPIView
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('health/', views.health, name='health'),
     path('albums/', views.list_albums, name='list_albums'),
     path('upload/', UploadPhotoAPIView.as_view(), name='upload_photo'),
     path('search/', SearchByFaceAPIView.as_view(), name='search_by_face'),
